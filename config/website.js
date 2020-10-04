81c3a3d3fb512696736c89776cbdb597eab4ed9f
Mitchell Stewart <mitchell.stewart08@gmail.com>
2020-10-04 07:46:21 -0500

    Add Author to Config

--- config/website.js
+++ config/website.js
@@ -7,7 +7,7 @@ module.exports = {
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
   siteDescription: 'Professional and personal blog of Mitch Stewart, a Texas based full-stack Javascript developer',
-  author: 'Author', // Author for schemaORGJSONLD
+  author: 'Mitchell Stewart', // Author for schemaORGJSONLD
   organization: '',
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
